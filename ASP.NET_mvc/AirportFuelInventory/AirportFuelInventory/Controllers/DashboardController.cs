59c7d0ab9e6d9b777dc85532b87f18b260f032f6
Manisha Parija <Manisha.P@mindfiresolutions.com>
2024-03-12 10:19:47 +0530

    Airport Fuel Inventory Completed

--- ASP.NET_mvc/AirportFuelInventory/AirportFuelInventory/Controllers/DashboardController.cs
+++ ASP.NET_mvc/AirportFuelInventory/AirportFuelInventory/Controllers/DashboardController.cs
@@ -13,7 +13,7 @@ namespace AirportFuelInventory.Controllers
     {
         // GET: Dashboard
 
-        public ActionResult Dashboard()
+        public ActionResult Dashboard(int? page, string sortColumn, string sortDirection)
         {
             var availableFuelData = AirportLogic.GetAvailableFuel();
 
@@ -24,6 +24,11 @@ namespace AirportFuelInventory.Controllers
         {
             var report = new ActionAsPdf("Dashboard");
             return report;
+        }
+
+        private string ToggleSortDirection(string currentDirection)
+        {
+            return currentDirection?.ToUpper() == "ASC" ? "DESC" : "ASC";
         }
     }
 }
